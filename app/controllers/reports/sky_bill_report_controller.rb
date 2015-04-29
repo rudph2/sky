275882d227aae1f80d2fe9dc51e7e68f0299a404
Sebastian Siemianowski <sebastian.siemianowski@sage.com>
2015-04-29 16:41:35 +0100

    removes the bug with uninitialized sky_bill_reporting_service

--- app/controllers/reports/sky_bill_report_controller.rb
+++ app/controllers/reports/sky_bill_report_controller.rb
@@ -1,7 +1,7 @@
 module Reports
   class SkyBillReportController < ApplicationController
     def index
-      sky_bill_reporting_service = ::Sky::Services::SkyBillReportingService.new
+      sky_bill_reporting_service = ::Sky::Services::SkyBillReportingService.new({})
       @report = sky_bill_reporting_service.build_report
     end
   end
