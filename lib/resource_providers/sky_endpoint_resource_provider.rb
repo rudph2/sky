d58f11caf56ec0a1619bd20cdd20245ab8177011
Sebastian Siemianowski <sebastian.siemianowski@sage.com>
2015-04-29 11:13:35 +0100

    modifies sky_endpoint_resource_provider to use dependency injection, adds the tests

--- lib/resource_providers/sky_endpoint_resource_provider.rb
+++ lib/resource_providers/sky_endpoint_resource_provider.rb
@@ -1,13 +1,12 @@
 module Sky
   module ResourceProviders
     class SkyEndpointResourceProvider
-      def initialize
-        @sky_endpoint_adapter = ::Sky::Adapters::SkyEndpointAdapter.new
+      def initialize(dependencies)
+        @endpoint_adapter = dependencies[:endpoint_adapter] || ::Sky::Adapters::SkyEndpointAdapter.new({})
       end
 
       def get_bill
-        result = @sky_endpoint_adapter.get_response_for("bill.json")
-
+        result = @endpoint_adapter.get_response_for("bill.json")
         JSON.parse( result, {:symbolize_names => true} )
       end
     end
--- spec/lib/resource_providers/sky_endpoint_resource_provider_spec.rb
+++ spec/lib/resource_providers/sky_endpoint_resource_provider_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe SkyEndpointResourceProvider do
+describe ::Sky::ResourceProviders::SkyEndpointResourceProvider do
 
+  let(:endpoint_adapter){double(:endpoint_adapter)}
+
+  subject{::Sky::ResourceProviders::SkyEndpointResourceProvider.new({:endpoint_adapter => endpoint_adapter})}
+
+  it 'returns correct json content' do
+    allow(endpoint_adapter).to receive(:get_response_for).with('bill.json').and_return('{"some_key": "some_value"}')
+    expect(subject.get_bill).to eq({:some_key => 'some_value'})
+  end
 end
