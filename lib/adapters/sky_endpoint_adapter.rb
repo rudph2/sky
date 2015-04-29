be2ee3ca7573343615fd39349163ba166b017299
Sebastian Siemianowski <sebastian.siemianowski@sage.com>
2015-04-29 10:40:24 +0100

    modifies sky-endpoint_adapter to use dependency injection, adds the tests

--- lib/adapters/sky_endpoint_adapter.rb
+++ lib/adapters/sky_endpoint_adapter.rb
@@ -1,14 +1,18 @@
 module Sky
   module Adapters
     class SkyEndpointAdapter
-      def get_response_for(endpoint)
-        conn = Faraday.new(:url => 'http://safe-plains-5453.herokuapp.com/bill.json') do |faraday|
-          faraday.request  :url_encoded
-          faraday.response :logger
-          faraday.adapter  Faraday.default_adapter
-        end
+      def initialize(dependencies)
+        @url = dependencies[:url] || 'http://safe-plains-5453.herokuapp.com/bill.json'
+        @default_adapter = dependencies[:default_adapter] || Faraday.default_adapter
+        @faraday = dependencies[:faraday] || Faraday.new(@url)
+      end
 
-        response = conn.get "/#{endpoint}"
+      def get_response_for(endpoint)
+        @faraday.request  :url_encoded
+        @faraday.response :logger
+        @faraday.adapter  @default_adapter
+
+        response = @faraday.get "/#{endpoint}"
         response.body
       end
     end
--- spec/lib/adapters/sky_endpoint_adapter_spec.rb
+++ spec/lib/adapters/sky_endpoint_adapter_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
-describe SkyEndpointAdapter do
+describe Sky::Adapters::SkyEndpointAdapter do
 
+  let(:faraday){double(:faraday)}
+  let(:url){'test_url'}
+  let(:default_adapter){'default_adapter'}
+  let(:response){double(:response)}
+
+  subject {Sky::Adapters::SkyEndpointAdapter.new(:faraday => faraday, :url => url, :default_adapter => default_adapter)}
+
+  before :each do
+    allow(faraday).to receive(:request).with(:url_encoded)
+    allow(faraday).to receive(:response).with(:logger)
+    allow(faraday).to receive(:adapter).with(default_adapter)
+  end
+
+  it 'gets the response for correct url' do
+    allow(faraday).to receive(:get).with('/foo_endpoint').and_return(response)
+    allow(response).to receive(:body).and_return('test_response')
+    expect(subject.get_response_for('foo_endpoint')).to eq 'test_response'
+  end
 end
